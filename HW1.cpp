206764134779a1502ea091077235576f74b90e2f
Limbodancer23 <83474450+Limbodancer23@users.noreply.github.com>
2024-03-13 22:41:58 +0500

    Добавьте файлы проекта.

---
+++ HW1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+inline void Print(string& str) {
+    cout << str << endl;
+}
+
+int main()
+{
+    string word = "Hellp world!";
+    Print(word);
+
+    int x = 100, y = x + 100, mult = x * y;
+
+    int b = 0;
+    b += 2;
+    int test;
+    int test2 = 100500;
+
+    int random = 1002;
+    cout << random;
+}
